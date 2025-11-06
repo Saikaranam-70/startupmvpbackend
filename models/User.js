88c1495bc204bdd10a8e2b310291354f69e89324
Saikaranam-70 <saikaranam46@gmail.com>
2025-11-06 21:54:27 +0530

    update

--- models/User.js
+++ models/User.js
@@ -3,21 +3,22 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     phone: {
-  type: String,
-  validate: {
-    validator: function (v) {
-      return /^\+?[1-9]\d{9,14}$/.test(v); // supports +91...
+      type: String,
+      validate: {
+        validator: function (v) {
+          return /^\+?[1-9]\d{9,14}$/.test(v); // supports +91...
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+      required: true,
     },
-    message: props => `${props.value} is not a valid phone number!`
-  },
-  required: true
-}
-,
     address: {
       type: String,
       required: true,
       trim: true,
     },
+    chatState: { type: String, default: null }, // e.g. "WAITING_FOR_FOOD_DETAILS"
+    tempData: { type: Object, default: {} }, // to hold temporary info
   },
   {
     timestamps: true, // adds createdAt and updatedAt
