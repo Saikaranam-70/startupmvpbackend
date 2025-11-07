4fbe92418ccbe53909379b63896f2a289d210e44
Saikaranam-70 <saikaranam46@gmail.com>
2025-11-07 20:00:21 +0530

    update

--- controllers/whatsappController.js
+++ controllers/whatsappController.js
@@ -251,6 +251,7 @@ const Order = require("../models/Order");
 const Agent = require("../models/Agent");
 const { findOrCreateUser } = require("./userController");
 const redis = require("../config/redis");   
+const NodeCache = require("node-cache");
 const localCache = new NodeCache({ stdTTL: 60 });
 require("dotenv").config();
 
