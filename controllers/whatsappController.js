0ff41c5049ced0bd39140d7625274610e79e42bc
Saikaranam-70 <saikaranam46@gmail.com>
2025-12-15 22:53:59 +0530

    update

--- controllers/whatsappController.js
+++ controllers/whatsappController.js
@@ -454,26 +454,26 @@ exports.receiveMessage = async (req, res) => {
       // if (c._id === "Main Course") {
       //   title = "Main Course(Biryanies)";
       // }
-      switch (c._id) {
-        case "Chinese":
-          title = "Chinese (Soups)";
-          break;
-        case "Fast Food":
-          title = "Fast Food (Noodles)";
-          break;
-        case "Snacks":
-          title = "Snacks (Samosa)";
-          break;
-        case "Starters":
-          title = "Starters (Chicken Fry)";
-          break;
-        case "Curry":
-          title = "Curry (Chicken)";
-          break;
-        case "Main Course":
-          title = "Main Course(Biryanies)";
-          break;
-      }
+      // switch (c._id) {
+      //   case "Chinese":
+      //     title = "Chinese (Soups)";
+      //     break;
+      //   case "Fast Food":
+      //     title = "Fast Food (Noodles)";
+      //     break;
+      //   case "Snacks":
+      //     title = "Snacks (Samosa)";
+      //     break;
+      //   case "Starters":
+      //     title = "Starters (Chicken Fry)";
+      //     break;
+      //   case "Curry":
+      //     title = "Curry (Chicken)";
+      //     break;
+      //   case "Main Course":
+      //     title = "Main Course(Biryanies)";
+      //     break;
+      // }
 
       return {
         id: `CAT_${c._id}`,
